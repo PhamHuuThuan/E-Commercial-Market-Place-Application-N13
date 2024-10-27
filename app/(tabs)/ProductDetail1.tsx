e930ddb123dbbc8aba7d82b96e044450cbfd5718
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2024-10-27 21:59:13 +0700

    Button cart and buy ProductDetail1

--- app/(tabs)/ProductDetail1.tsx
+++ app/(tabs)/ProductDetail1.tsx
@@ -32,8 +32,14 @@ const ProductDetail = () => {
                 <Text style={stylesLocal.reviewText}>(99 reviews)</Text>
             </View>
         </View>
-        <View>
-            <Image />
+        <View style={{width: '100%', flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 20, paddingTop: 20, borderTopWidth: 1, borderTopColor: 'rgba(236, 236, 236, 1)'}}>
+            <Pressable style={{width: 40, height: 40, borderColor: 'rgba(0, 224, 255, 1)', borderWidth: 0.5, borderRadius: 5, justifyContent: 'center', alignItems: 'center'}}>
+                <Image source={{uri: 'https://s3-alpha-sig.figma.com/img/f098/e60e/4b26942409e13aa2c3dda427580702b3?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=q3uVQM0AkMFsyQ9V3G~U1Q7w96fHwnNK2rtXtP7mRVVYti-N1WcO1azHVDoylUSA6A2fiKKtSKNm6ZQzsijW7b1VnCiPv-5sEmgf36t8IR4cTyQDnVKEi6ytaqRVlID2HUHgseKQGFjzAIHhfN8Jn9emyL1VWJIM-88m6VrTlif-n6SHR1zJYao6sX7U2I754mYoGM9AiEFk7BqzK-~ltD44R9JWCECX-O5yV7BuGtg8IGEhoEAlgwdTjDQyJP0CyPIJ7S-xcUXK8FRMNAKofJ7v3oZa8rWh-QgmuudpHilpzYZVlw-zp14KW1f-wvQtbsirCRmUQCw7kn6z9QoarQ__'}} 
+                style={{width: 24, height: 24}}/>
+            </Pressable>
+            <Pressable style={{height: 40, width: '70%', backgroundColor: 'rgba(0, 224, 255, 1)', borderRadius: 5, justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{color: 'white'}}>Buy now</Text>
+            </Pressable>
         </View>
     </SafeAreaView>
   );
